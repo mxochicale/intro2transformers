b56b639fba4ea3ce3f37d5ff5beaea858c62159b
Miguel Xochicale <perez.xochicale@gmail.com>
2024-12-30 09:14:43 +0000

    adds precommit and tests

---
+++ tests/test_resources.py
@@ -0,0 +1,22 @@
+import os
+from loguru import logger
+import torch
+import gc
+
+def test_gpu():
+    """
+    Test data path
+    pytest -vs tests/test_resources.py::test_gpu
+
+    """
+    os.system("nvidia-smi")
+
+    logger.info(gc.collect())
+
+    if torch.cuda.is_available():
+        logger.info("GPU available: " + torch.cuda.get_device_name(0))
+        logger.info("GPU count: " + str(torch.cuda.device_count()))
+    else:
+        logger.info(torch.cuda.is_available())
+        logger.info("GPU not available 😔")
+
